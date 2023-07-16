615a3ed0bdab02435f96540c714d038cf6028805
SetoSKins <102343110+SetoSKins@users.noreply.github.com>
2023-07-17 01:32:25 +0800

    A1

---
+++ SetoSkins/customize.sh
@@ -0,0 +1,63 @@
+file2=$(ls /sys/class/power_supply/battery/*charge_current /sys/class/power_supply/battery/current_max /sys/class/power_supply/battery/thermal_input_current 2>>/dev/null|tr -d '\n')
+if [ ! -f "$file2" ]; then
+echo "- 不支持你的设备"
+exit 1
+fi
+Reserve() {
+	echo "- 是否保留之前配置"
+	echo "- 音量上键为保留"
+	echo "- 音量下键为取消"
+	key_click=""
+	while [ "$key_click" = "" ]; do
+		key_click="$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_')"
+		sleep 0.2
+	done
+	case "$key_click" in
+	"KEY_VOLUMEUP")
+		echo "- 确认保留"
+		sleep 1
+		cp /data/adb/modules/SetoCharge/配置.prop /data/adb/配置.prop
+		cp /data/adb/modules/SetoCharge/游戏名单.prop /data/adb/游戏名单.prop
+		if [ ! -f "/data/adb/配置.prop" ]; then
+			echo "- 正在持续写入保留配置文件 请耐心等待"
+			for i in $(seq 1 60); do
+				sleep 1
+				if [ ! -f "/data/adb/配置.prop" ]; then
+					cp /data/adb/modules/SetoCharge/配置.prop /data/adb/配置.prop
+					if [ -f "/data/adb/配置.prop" ]; then
+						break
+					fi
+				fi
+			done
+		fi
+		if [ ! -f "/data/adb/游戏名单.prop" ]; then
+			echo "- 正在持续写入保留配置文件 请耐心等待"
+			for i in $(seq 1 60); do
+				sleep 1
+				if [ ! -f "/data/adb/游戏名单.prop" ]; then
+					cp /data/adb/modules/SetoCharge/游戏名单.prop /data/adb/游戏名单.prop
+					if [ -f "/data/adb/游戏名单.prop" ] || [ ! -f "/data/adb/modules/SetoCharge/游戏名单.prop" ]; then
+						break
+					fi
+				fi
+			done
+		fi
+		;;
+	*)
+		echo "- 取消保留"
+		;;
+	esac
+}
+if [ -d "/data/adb/modules/SetoCharge/" ]; then
+	echo "- 检测到有备份配置 鉴定为更新模块"
+Reserve
+else
+	echo "- 第一次安装本模块请看好说明"
+fi
+echo "- 配置在模块根目录"
+echo "- 请卸载一切有关于改电流大小的模块 否则模块不会生效"
+sleep 5
+coolapkTesting=$(pm list package | grep -w 'com.coolapk.market')
+if [[ $coolapkTesting != "" ]] && [ ! -d "/data/adb/modules/SetoCharge/" ]; then
+	am start -d 'coolmarket://u/5562122' >/dev/null 2>&1
+fi
